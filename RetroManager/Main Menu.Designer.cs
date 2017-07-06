0e659632a6e97a7382b7a246d308beb619aa06a1
ZestyTS <ZestyTS@gmail.com>
2017-07-06 16:16:48 -0400

    Gave descriptive names to MainMenu's form entities

--- RetroManager/Main Menu.Designer.cs
+++ RetroManager/Main Menu.Designer.cs
@@ -28,34 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnDuplicate = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnDuplicate
             // 
-            this.button1.Location = new System.Drawing.Point(76, 44);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Duplicate Remover";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnDuplicate.Location = new System.Drawing.Point(76, 44);
+            this.btnDuplicate.Name = "btnDuplicate";
+            this.btnDuplicate.Size = new System.Drawing.Size(118, 23);
+            this.btnDuplicate.TabIndex = 0;
+            this.btnDuplicate.Text = "Duplicate Remover";
+            this.btnDuplicate.UseVisualStyleBackColor = true;
+            this.btnDuplicate.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.btnDuplicate);
+            this.Name = "MainMenu";
+            this.Text = "Main Menu";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDuplicate;
     }
 }
 
