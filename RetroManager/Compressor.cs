630a6b043a60e1ac119285f59c071cb9111cd81d
ZestyTS <ZestyTS@gmail.com>
2017-07-28 17:44:52 -0400

    Removed unused directories

--- RetroManager/Compressor.cs
+++ RetroManager/Compressor.cs
@@ -1,8 +1,4 @@
 ﻿﻿﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using SevenZip.Compression.LZMA;
 using System.IO;
 using SevenZip;
 using SharpCompress.Archives.SevenZip;
